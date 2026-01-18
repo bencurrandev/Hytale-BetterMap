605cac23a980078fb89746b0e43011f8734336f7
Louis Rollet <louis.rollet@epitech.eu>
2026-01-19 05:16:33 +0800

    [FIX] remove global waypoint for now

--- src/main/java/dev/ninesliced/ui/WaypointEditPage.java
+++ src/main/java/dev/ninesliced/ui/WaypointEditPage.java
@@ -84,11 +84,11 @@ public class WaypointEditPage extends InteractiveCustomUIPage<WaypointEditPage.E
         ui.set("#SelBlue.Visible", "BlueMarker.png".equals(this.selectedColor));
         ui.set("#SelWhite.Visible", "Coordinate.png".equals(this.selectedColor));
 
-        boolean canGlobal = PermissionsUtil.canUseGlobalWaypoints(player);
-        ui.set("#GlobalRow.Visible", canGlobal);
-        if (canGlobal) {
-            ui.set("#GlobalCheckbox.Value", this.global);
-        }
+        //boolean canGlobal = PermissionsUtil.canUseGlobalWaypoints(player);
+        //ui.set("#GlobalRow.Visible", canGlobal);
+        //if (canGlobal) {
+        //    ui.set("#GlobalCheckbox.Value", this.global);
+        //}
 
         events.addEventBinding(CustomUIEventBindingType.ValueChanged, "#NameInput",  
             new EventData().put(EditData.KEY_NAME_INPUT, "#NameInput.Value"), false);
